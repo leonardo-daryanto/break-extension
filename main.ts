cbb0310df1ffae8f004210461a2639c67acf220e
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-08-03 14:43:45 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
-})
+namespace loops {
+    /**
+     *  breaks a loop
+     */
+    //% block
+    //% group=" On break"
+    export function breakLoop(body: () => void): void {
+        while (true) {
+           if (randint(0, 10) < 5) {
+               break; 
+            }
+        } 
+    }
+}
