245eb4f26fa52b559b47e0703c5252fbdb650a5a
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-08-03 14:48:09 +0700

    Update main.blocks, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+loops.breakLoop(function() {
+    loop()
+})
+function loop() {
+    
+    for (let i = 0; i < 4; i++) {
+        
+        basic.showIcon(IconNames.Heart)
+    }
+
+    basic.showIcon(IconNames.Yes)
+}
